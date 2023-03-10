08826c146819b974bc6aa4ecb197ba9fe1a7fe7e
Alaa Adel <alaaadelahmed50@gmail.com>
2023-03-10 15:13:04 +0200

    initial

---
+++ functions/hello.js
@@ -0,0 +1,13 @@
+const items = [
+    {id:1, name:"jhon"},
+    {id:2, name:"susan"}
+]
+
+
+
+exports.handler= async function(event,context){
+  return {
+    statusCode:200,
+    body:"Hello World"
+  }  
+}
